5081b5237d971312e28a37632b34cc6c1f122d12
conanlm <conanlm3@gmail.com>
2019-04-16 19:41:43 +0800

    序列从id文件中读取

--- PyUserInput.py
+++ PyUserInput.py
@@ -21,7 +21,10 @@ k = PyKeyboard()
 # k.type_string('Hello, World!')
 
 print(m.position()) #显示鼠标坐标
-n=631
+f=open("id.txt")
+n= int(f.read())
+f.close()
+# n=1025
 
 # 创建(269, 142) 
 # 地址名称坐标 (417, 183)
@@ -30,21 +33,59 @@ n=631
 
 
 # 循环读取ip
+# for line in open("ip.txt"):  
+#     #print line,  #python2 用法
+#     m.click(269, 142, 1)
+#     time.sleep( 1 )
+#     m.click(417, 183, 1)
+#     time.sleep( 0.5 )
+#     k.type_string('neweyip'+str(n))
+#     time.sleep( 1 )
+#     m.click(400, 254, 1)
+#     time.sleep( 0.5 )
+#     k.type_string(line.strip())
+#     m.click(1000, 376, 1)
+#     n=n+1
+#     time.sleep( 1 )
+#     # print(line)
+
+
+# 家中vpn使用坐标
+# 循环读取ip
+# for line in open("ip.txt"):  
+#     #print line,  #python2 用法
+#     m.click(289, 146, 1)
+#     time.sleep( 1 )
+#     m.click(456, 180, 1)
+#     time.sleep( 0.5 )
+#     k.type_string('neweyip'+str(n))
+#     time.sleep( 1 )
+#     m.click(486, 255, 1)
+#     time.sleep( 0.5 )
+#     k.type_string(line.strip())
+#     m.click(669, 374, 1)
+#     n=n+1
+#     time.sleep( 1 )
+#     # print(line)
+
+# 基地使用坐标
 for line in open("ip.txt"):  
     #print line,  #python2 用法
-    m.click(269, 142, 1)
+    m.click(288, 147, 1)
     time.sleep( 1 )
-    m.click(417, 183, 1)
+    m.click(442, 178, 1)
+    time.sleep( 0.5 )
+    k.type_string('neweyip'+str(n))
     time.sleep( 1 )
-    k.type_string('eyip'+str(n))
-    time.sleep( 1 )
-    m.click(400, 254, 1)
-    time.sleep( 1 )
+    m.click(466, 250, 1)
+    time.sleep( 0.5 )
     k.type_string(line.strip())
-    m.click(1000, 376, 1)
+    m.click(663, 375, 1)
     n=n+1
     time.sleep( 1 )
     # print(line)
+
+
 
 
     # for line in open("ip.txt"):  
@@ -68,3 +109,7 @@ for line in open("ip.txt"):
     # # n=n+1
     # # time.sleep( 1 )
     # # print(line)
+
+f = open('id.txt', 'w')
+f.write(str(n))
+f.close()
